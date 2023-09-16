422245f6b32ee8a154b6d50845b167e15c9b9810
Ritika Pathak <ritika8949@gmail.com>
2023-09-17 01:21:08 +0530

    TMDB API data Fetch && built MainContainer

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,11 +1,26 @@
 import Header from "./Header";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+    useNowPlayingMovies();
+
+    return (
+        <div>
+            <Header />
+            <MainContainer />
+            <SecondaryContainer />
+            {/* 
+              MainContainer
+                - VideoBackground
+                - VideoTitle
+              SecondaryContainer
+                - MovieList * n
+                  - cards * n
+            */}
+        </div>
+    );
 };
 
 export default Browse;
