9ed752c911e1c8af7d1583d47fd7e6e3e9f48f5a
Ritika Pathak <ritika8949@gmail.com>
2023-09-17 20:31:21 +0530

    Made the app responsive for mobile

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -12,9 +12,9 @@ const MainContainer = () => {
     const { original_title, overview, id } = mainMovie;
 
     return (
-        <div>
-        <VideoTitle title={original_title} overview={overview} />
-        <VideoBackground movieId={id} />
+        <div className="pt-[30%] bg-black md:pt-0">
+            <VideoTitle title={original_title} overview={overview} />
+            <VideoBackground movieId={id} />
         </div>
     );
 };
